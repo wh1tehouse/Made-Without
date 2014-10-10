becac28f80449e58720d75facb392dd36a8b8b7b
wh1tehouse <d.whitehouse@logicalinternetsolutions.com>
2014-10-10 23:50:55 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -49,18 +49,17 @@ var app = {
     scan: function() {
         console.log('scanning');
         try {
-            window.plugins.barcodeScanner.scan(function(args) {
-                console.log("Scanner result: \n" +
-                    "text: " + args.text + "\n" +
-                    "format: " + args.format + "\n" +
-                    "cancelled: " + args.cancelled + "\n");
-                /*
-                if (args.format == "QR_CODE") {
-                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-                }
-                */
-                document.getElementById("info").innerHTML = args.text;
-                console.log(args);
+            cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
         });
         } catch (ex) {
             console.log(ex.message);
