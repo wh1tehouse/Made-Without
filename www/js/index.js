f25fd99393d8da4b7a72f9cffc70774b7c6f91f7
wh1tehouse <d.whitehouse@logicalinternetsolutions.com>
2014-10-10 23:09:29 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -46,4 +46,24 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+    scan: function() {
+        console.log('scanning');
+        try {
+            window.plugins.barcodeScanner.scan(function(args) {
+                console.log("Scanner result: \n" +
+                    "text: " + args.text + "\n" +
+                    "format: " + args.format + "\n" +
+                    "cancelled: " + args.cancelled + "\n");
+                /*
+                if (args.format == "QR_CODE") {
+                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                }
+                */
+                document.getElementById("info").innerHTML = args.text;
+                console.log(args);
+        });
+        } catch (ex) {
+            console.log(ex.message);
+        }
+    }
 };
