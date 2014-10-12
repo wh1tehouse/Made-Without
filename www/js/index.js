dbf7900f1ad2ba2990fcfa2819b43fd5feb1ca7b
wh1tehouse <d.whitehouse@logicalinternetsolutions.com>
2014-10-13 00:37:26 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -78,23 +78,5 @@ scan: function() {
         }, function (error) { 
             console.log("Scanning failed: ", error); 
         } );
-    },
-    onSuccess: function(position) {
-        var element = document.getElementById('geolocation');
-        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            'Longitude: '          + position.coords.longitude             + '<br />' +
-                            'Altitude: '           + position.coords.altitude              + '<br />' +
-                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                            'Heading: '            + position.coords.heading               + '<br />' +
-                            'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          + position.timestamp                    + '<br />';
-    }
-
-    // onError Callback receives a PositionError object
-    //
-    onError: function(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
     }
 };
