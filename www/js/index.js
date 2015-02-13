197ca0524c43f00d1bb8dc9b3416b2b3241182c5
David Whitehouse <d.whitehouse@logicalinternetsolutions.com>
2015-02-13 23:16:06 +0000

    edit

--- www/js/index.js
+++ www/js/index.js
@@ -87,14 +87,14 @@ scan: function() {
 //
 function onSuccess(position) {
     var element = document.getElementById('geolocation');
-    element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                        'Longitude: '          + position.coords.longitude             + '<br />' +
-                        'Altitude: '           + position.coords.altitude              + '<br />' +
-                        'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                        'Heading: '            + position.coords.heading               + '<br />' +
-                        'Speed: '              + position.coords.speed                 + '<br />' +
-                        'Timestamp: '          + position.timestamp                    + '<br />';
+    //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+    //                    'Longitude: '          + position.coords.longitude             + '<br />' +
+    //                    'Altitude: '           + position.coords.altitude              + '<br />' +
+    //                    'Accuracy: '           + position.coords.accuracy              + '<br />' +
+    //                    'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+    //                    'Heading: '            + position.coords.heading               + '<br />' +
+    //                    'Speed: '              + position.coords.speed                 + '<br />' +
+    //                    'Timestamp: '          + position.timestamp                    + '<br />';
 }
 
 // onError Callback receives a PositionError object
